5c667377cacae6cde3ed6956d66ccdc4f62163f8
yussan <yussandeveloper@gmail.com>
2018-05-23 07:59:47 +0700

    wip browse endpoint

--- src/v2/transformers/competition.py
+++ src/v2/transformers/competition.py
@@ -2,8 +2,25 @@ from v2.helpers.encId import encId
 
 def transform(n):
   return {
-    'id': n.id_kompetisi,
-    'enc_id': encId(n.id_kompetisi),
+    'id': encId(n.id_kompetisi),
+    'title': n.judul_kompetisi,
+    'sort': n.sort,
+    'penyelenggara': n.penyelenggara,
+    'deadline_at': n.deadline.strftime('%s'),
+    'announcement_at': n.pengumuman.strftime('%s'),
+    'created_at': n.created_at.strftime('%s'),
+    'updated_at': n.updated_at.strftime('%s'),
+    'is_mediapartner': n.mediapartner == 1,
+    'is_garansi': n.garansi == 1,
+    'poster': '',
+    'main_category': {
+      'id': n.id_main_kat,
+      'name': n.main_kat,
+    },
+    'sub_category': {
+      'id': n.id_sub_kat,
+      'name': n.sub_kat
+    },
     'user': {
       'id': 1,
       'username': n.username,
