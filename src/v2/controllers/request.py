970432e582f6b192ae9fda0bba5f9330be62532c
yussan <yussandeveloper@gmail.com>
2018-06-06 04:46:07 +0700

    added thanks email after send competition

--- src/v2/controllers/request.py
+++ src/v2/controllers/request.py
@@ -6,6 +6,7 @@ from flask_restful import Resource, Api
 from wtforms import Form, BooleanField, StringField, PasswordField, validators, FileField
 # from flask_mail import Message
 from v2.modules.file_upload import handleUpload
+from v2.modules.mail import sendEmail
 from v2.models.request import getRequest, getRequestById, insertRequest, updateRequest
 from v2.helpers.response import apiResponse
 from v2.transformers.request import transform
@@ -71,7 +72,12 @@ class RequestApi(Resource):
                 'note': ''
             }
 
+            # insert data
             insertRequest(params)
+
+            # email thanks
+            sendEmail('Terimakasih Telah Mengirim Kompetisi - kompetisi.id',
+                  EmailThanksBody, [params['email']])
 
             return apiResponse(201, 'Kompetisi kamu akan dicek oleh moderator, status selanjutkan akan kami kirim via email'), 201
         else: 
@@ -122,3 +128,35 @@ api_request = Api(api_request_bp)
 # routes
 api_request.add_resource(RequestApi, '/v2/request')
 api_request.add_resource(RequestAction, '/v2/request/action/<int:id>')
+
+EmailThanksBody = """
+<div class="">
+        <!--[if mso]><table width="100%" cellpadding="0" cellspacing="0" border="0"><tr><td style="padding-right: 30px; padding-left: 30px; padding-top: 30px; padding-bottom: 15px;"><![endif]-->
+        <div style="color:#555555;font-family:'Montserrat', 'Trebuchet MS', 'Lucida Grande', 'Lucida Sans Unicode', 'Lucida Sans', Tahoma, sans-serif;line-height:120%; padding-right: 30px; padding-left: 30px; padding-top: 30px; padding-bottom: 15px;">
+            <div style="font-family:Montserrat, 'Trebuchet MS', 'Lucida Grande', 'Lucida Sans Unicode', 'Lucida Sans', Tahoma, sans-serif;font-size:12px;line-height:14px;color:#555555;text-align:left;">
+            <p style="margin: 0;font-size: 12px;line-height: 14px;text-align: center">
+                <span style="font-size: 18px; line-height: 21px;">
+                <strong>Terimakasih Telah Mengirim Kompetisi</strong>
+                </span>
+            </p>
+            </div>
+        </div>
+        <!--[if mso]></td></tr></table><![endif]-->
+        </div>
+
+        <div class="">
+        <!--[if mso]><table width="100%" cellpadding="0" cellspacing="0" border="0"><tr><td style="padding-right: 10px; padding-left: 10px; padding-top: 10px; padding-bottom: 30px;"><![endif]-->
+        <div style="color:#989898;font-family:Arial, 'Helvetica Neue', Helvetica, sans-serif;line-height:120%; padding-right: 10px; padding-left: 10px; padding-top: 10px; padding-bottom: 30px;">
+            <div style="font-size:12px;line-height:14px;color:#989898;font-family:Arial, 'Helvetica Neue', Helvetica, sans-serif;text-align:left;">
+            <p style="margin: 0;font-size: 14px;line-height: 17px;text-align: center">
+                Kompetisi yang kamu pasang akan divalidasi oleh moderator, kamu akan menerima email balasan berupa status aksi untuk kompetisi ini.
+            </p>
+            </div>
+        </div>
+        <!--[if mso]></td></tr></table><![endif]-->
+        </div>
+
+        <!--[if (!mso)&(!IE)]><!-->
+    </div>
+"""
+
--- src/v2/modules/mail.py
+++ src/v2/modules/mail.py
@@ -1,11 +1,11 @@
 import app 
 from flask_mail import Message
-from flask import render_template
+from flask import render_template, Markup
 
 def sendEmail(title, body, target):
   # ref: https://pythonhosted.org/Flask-Mail/
   msg = Message(title,
                 sender="noreply@kompetisi.id",
                 recipients=target)
-  msg.html = render_template('email/test.html')
+  msg.html = render_template('email/index.html', body=Markup(body))
   return app.mail.send(msg)
