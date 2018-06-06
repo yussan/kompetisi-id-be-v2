ff96f3a6e2fae9a6e5c67d9f9a6f8a2fbf8b54d1
yussan <yussandeveloper@gmail.com>
2018-06-06 20:27:08 +0700

    fixing email issue on request

--- src/v2/controllers/request.py
+++ src/v2/controllers/request.py
@@ -12,6 +12,37 @@ from v2.helpers.response import apiResponse
 from v2.transformers.request import transform
 from v2.middlewares.auth import isModerator
 
+EmailThanksBody = """
+<div class="">
+        <!--[if mso]><table width="100%" cellpadding="0" cellspacing="0" border="0"><tr><td style="padding-right: 30px; padding-left: 30px; padding-top: 30px; padding-bottom: 15px;"><![endif]-->
+        <div style="color:#555555;font-family:'Montserrat', 'Trebuchet MS', 'Lucida Grande', 'Lucida Sans Unicode', 'Lucida Sans', Tahoma, sans-serif;line-height:120%; padding-right: 30px; padding-left: 30px; padding-top: 30px; padding-bottom: 15px;">
+            <div style="font-family:Montserrat, 'Trebuchet MS', 'Lucida Grande', 'Lucida Sans Unicode', 'Lucida Sans', Tahoma, sans-serif;font-size:12px;line-height:14px;color:#555555;text-align:left;">
+            <p style="margin: 0;font-size: 12px;line-height: 14px;text-align: center">
+                <span style="font-size: 18px; line-height: 21px;">
+                <strong>Terimakasih Telah Mengirim Kompetisi</strong>
+                </span>
+            </p>
+            </div>
+        </div>
+        <!--[if mso]></td></tr></table><![endif]-->
+        </div>
+
+        <div class="">
+        <!--[if mso]><table width="100%" cellpadding="0" cellspacing="0" border="0"><tr><td style="padding-right: 10px; padding-left: 10px; padding-top: 10px; padding-bottom: 30px;"><![endif]-->
+        <div style="color:#989898;font-family:Arial, 'Helvetica Neue', Helvetica, sans-serif;line-height:120%; padding-right: 10px; padding-left: 10px; padding-top: 10px; padding-bottom: 30px;">
+            <div style="font-size:12px;line-height:14px;color:#989898;font-family:Arial, 'Helvetica Neue', Helvetica, sans-serif;text-align:left;">
+            <p style="margin: 0;font-size: 14px;line-height: 17px;text-align: center">
+                Kompetisi yang kamu kirim dengan judul "{}" akan divalidasi oleh moderator, kamu akan menerima email balasan berupa status aksi untuk kompetisi ini.
+            </p>
+            </div>
+        </div>
+        <!--[if mso]></td></tr></table><![endif]-->
+        </div>
+
+        <!--[if (!mso)&(!IE)]><!-->
+    </div>
+"""
+
 # validations
 # ref: http://flask.pocoo.org/docs/0.12/patterns/wtforms/
 class FormValidator(Form):
@@ -75,9 +106,12 @@ class RequestApi(Resource):
             # insert data
             insertRequest(params)
 
+
             # email thanks
+            # ref: https://www.digitalocean.com/community/tutorials/how-to-use-string-formatters-in-python-3
+            body = EmailThanksBody.format(params['nama'])
             sendEmail('Terimakasih Telah Mengirim Kompetisi - kompetisi.id',
-                  EmailThanksBody, [params['email']])
+                  body, [params['email']])
 
             return apiResponse(201, 'Kompetisi kamu akan dicek oleh moderator, status selanjutkan akan kami kirim via email'), 201
         else: 
@@ -107,9 +141,7 @@ class RequestAction(Resource):
                 updateRequest(params, id)
 
                 # send email to users
-                # msg = Message('Kompetisi ada Telah kami pasang', sender = 'noreply@kompetisi.id', recipients = [request_data['email']])
-                # msg.body =   'This is email body'
-                # mail.send(msg)
+                
                 
                 # json response
                 return apiResponse(200, 'Request berhasil di update'), 200
@@ -128,35 +160,4 @@ api_request = Api(api_request_bp)
 # routes
 api_request.add_resource(RequestApi, '/v2/request')
 api_request.add_resource(RequestAction, '/v2/request/action/<int:id>')
-
-EmailThanksBody = """
-<div class="">
-        <!--[if mso]><table width="100%" cellpadding="0" cellspacing="0" border="0"><tr><td style="padding-right: 30px; padding-left: 30px; padding-top: 30px; padding-bottom: 15px;"><![endif]-->
-        <div style="color:#555555;font-family:'Montserrat', 'Trebuchet MS', 'Lucida Grande', 'Lucida Sans Unicode', 'Lucida Sans', Tahoma, sans-serif;line-height:120%; padding-right: 30px; padding-left: 30px; padding-top: 30px; padding-bottom: 15px;">
-            <div style="font-family:Montserrat, 'Trebuchet MS', 'Lucida Grande', 'Lucida Sans Unicode', 'Lucida Sans', Tahoma, sans-serif;font-size:12px;line-height:14px;color:#555555;text-align:left;">
-            <p style="margin: 0;font-size: 12px;line-height: 14px;text-align: center">
-                <span style="font-size: 18px; line-height: 21px;">
-                <strong>Terimakasih Telah Mengirim Kompetisi</strong>
-                </span>
-            </p>
-            </div>
-        </div>
-        <!--[if mso]></td></tr></table><![endif]-->
-        </div>
-
-        <div class="">
-        <!--[if mso]><table width="100%" cellpadding="0" cellspacing="0" border="0"><tr><td style="padding-right: 10px; padding-left: 10px; padding-top: 10px; padding-bottom: 30px;"><![endif]-->
-        <div style="color:#989898;font-family:Arial, 'Helvetica Neue', Helvetica, sans-serif;line-height:120%; padding-right: 10px; padding-left: 10px; padding-top: 10px; padding-bottom: 30px;">
-            <div style="font-size:12px;line-height:14px;color:#989898;font-family:Arial, 'Helvetica Neue', Helvetica, sans-serif;text-align:left;">
-            <p style="margin: 0;font-size: 14px;line-height: 17px;text-align: center">
-                Kompetisi yang kamu pasang akan divalidasi oleh moderator, kamu akan menerima email balasan berupa status aksi untuk kompetisi ini.
-            </p>
-            </div>
-        </div>
-        <!--[if mso]></td></tr></table><![endif]-->
-        </div>
-
-        <!--[if (!mso)&(!IE)]><!-->
-    </div>
-"""
 
