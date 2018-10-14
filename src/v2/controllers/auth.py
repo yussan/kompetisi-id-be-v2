827432be442ff039b7eb60afe090e0c5142877ff
yussan <yussandeveloper@gmail.com>
2018-10-14 11:49:36 +0700

    feat(oauth): stable iauth login with fb and google

--- src/v2/controllers/auth.py
+++ src/v2/controllers/auth.py
@@ -2,28 +2,46 @@ import hashlib
 from flask import Blueprint, request
 from flask_restful import Api, Resource
 from v2.helpers.response import apiResponse
-from v2.models.users import login
+from v2.models.users import login, oauthLogin
 from v2.transformers.user import transform
 
-class Login(Resource):
-  def post(self):
-    # ref md5 hash: https://stackoverflow.com/a/5297495/2780875
-    params = {
-      'username': request.form.get('username'),
-      'password': hashlib.md5(request.form.get('password')).hexdigest()
-    }
 
-    result = login(params)
-    if result is not None:
-      return apiResponse(200,  'Login berhasil', {'data': transform(result)})
-    else:
-      return apiResponse(204)
+class Login(Resource):
+    def post(self):
+        # ref md5 hash: https://stackoverflow.com/a/5297495/2780875
+        params = {
+            'username': request.form.get('username'),
+            'password': hashlib.md5(request.form.get('password')).hexdigest()
+        }
+
+        result = login(params)
+        if result is not None:
+            return apiResponse(200,  'Login berhasil', {'data': transform(result)})
+        else:
+            return apiResponse(204)
+
 
 class Register(Resource):
-  def post(self):
-    return {}
+    def post(self):
+        return {}
+
+
+class OauthLogin(Resource):
+    def post(self):
+        params = {
+            'provider': request.form.get('provider'),
+            'user_id': request.form.get('user_id')
+        }
+
+        result = oauthLogin(params)
+        if result is not None:
+            return apiResponse(200,  'Login berhasil', {'data': transform(result)})
+        else:
+            return apiResponse(204, 'Akun tidak ditemukan, silahkan melakukan "registrasi" terlebih dahulu')
+
 
 api_auth_bp = Blueprint('api_auth', __name__)
 api_auth = Api(api_auth_bp)
 api_auth.add_resource(Login, '/v2/login')
 api_auth.add_resource(Register, '/v2/register')
+api_auth.add_resource(OauthLogin, '/v2/oauth/login')
--- src/v2/models/users.py
+++ src/v2/models/users.py
@@ -16,13 +16,35 @@ Users = Table('user', metadata,
               Column('is_verified', INT),
               )
 
-select_column = [Users.c.id_user, Users.c.username, Users.c.email, Users.c.fullname, Users.c.moto, Users.c.last_login, Users.c.status, Users.c.level, Users.c.is_verified]
+UsersOauth = Table('user_oauth', metadata,
+                  Column('id', BIGINT),
+                  Column('user_id', BIGINT),
+                  Column('provider', VARCHAR),
+                  Column('oauth_user_id', VARCHAR),
+                  Column('oauth_token', TEXT),
+                  )
+
+select_column_user = [Users.c.id_user, Users.c.username, Users.c.email, Users.c.fullname,
+                 Users.c.moto, Users.c.last_login, Users.c.status, Users.c.level, Users.c.is_verified]
+
+join_user = Users.join(UsersOauth, Users.c.id_user == UsersOauth.c.user_id)
 
 def login(params):
-  query = select(select_column)\
-    .select_from(Users)\
-    .where(or_(Users.c.username == params['username'], Users.c.email == params['username']))\
-    .where(Users.c.password == params['password'])
+    query = select(select_column_user)\
+        .select_from(Users)\
+        .where(or_(Users.c.username == params['username'], Users.c.email == params['username']))\
+        .where(Users.c.password == params['password'])
+
+    result = connection.execute(query).fetchone()
+
+    return result
+
+
+def oauthLogin(params):
+  query = select(select_column_user)\
+        .select_from(join_user)\
+        .where(UsersOauth.c.provider == params['provider'])\
+        .where(UsersOauth.c.oauth_user_id == params['user_id'])
 
   result = connection.execute(query).fetchone()
 
