6ca015d47bad19d8512360f74be0e4da86861057
Yusuf Akhsan Hidayat <yussan@Local-Computer.local>
2019-04-29 16:53:46 +0700

    merapikan

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-export $(cat .env)  && ./venv/bin/python ./src/run.py
+export $(cat .env) && ./venv/bin/python ./src/run.py
--- src/v2/modules/db.py
+++ src/v2/modules/db.py
@@ -9,11 +9,13 @@ db_password = os.environ.get('DB_PASSWORD', 'Rahasia42_')
 db_name = os.environ.get('DB_NAME', 'ki_dev42')
 
 # solved utf8 from: https://stackoverflow.com/questions/11972363/sqlalchemy-and-unicodedecodeerror?utm_medium=organic&utm_source=google_rich_qa&utm_campaign=google_rich_qa
-engine = create_engine('mysql+pymysql://' + db_user + ':' + db_password + '@' + db_host + '/' + db_name + '?charset=utf8', pool_recycle=3600)
+engine = create_engine('mysql+pymysql://' + db_user + ':' + db_password +
+                       '@' + db_host + '/' + db_name + '?charset=utf8', pool_recycle=3600)
 # print executed query
 
 # log option
-engine.echo = os.environ.get('FLASK_DEBUG') == 'True' or os.environ.get('FLASK_DEBUG') == True
+engine.echo = os.environ.get(
+    'FLASK_DEBUG') == 'True' or os.environ.get('FLASK_DEBUG') == True
 connection = engine.connect()
 connection = connection.execution_options(
     isolation_level="READ COMMITTED"
