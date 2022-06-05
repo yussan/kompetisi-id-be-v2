e3da232b150778568c75d6c203c583da08855058
yussan <main.yussan@gmail.com>
2022-06-05 20:39:44 +0700

    force revert email config

--- src/v2/modules/mail.py
+++ src/v2/modules/mail.py
@@ -5,22 +5,22 @@ from flask import render_template, Markup
 
 def sendEmail(title, body, target):
      # ref: https: // pythonhosted.org/Flask-Mail/
-    msg = Message(title,
-                  sender="noreply@kompetisi.id",
-                  recipients=target)
-    # msg.html = body
-    # location: /v2/email/index.html
-    msg.html = render_template(
-        'email/index.html', body=Markup(body))
-    return app.mail.send(msg)
-# try:
-#     # ref: https: // pythonhosted.org/Flask-Mail/
-#     msg = Message(title,
-#                   sender="noreply@kompetisi.id",
-#                   recipients=target)
-#     msg.html = render_template('email/index.html', body=Markup(body))
-#     return app.mail.send(msg)
-# except NameError:
-#     print("[ERROR] Error sending email....")
-#     print(NameError)
-#     return True
+    # msg = Message(title,
+    #               sender="noreply@kompetisi.id",
+    #               recipients=target)
+    # # msg.html = body
+    # # location: /v2/email/index.html
+    # msg.html = render_template(
+    #     'email/index.html', body=Markup(body))
+    # return app.mail.send(msg)
+    try:
+        # ref: https: // pythonhosted.org/Flask-Mail/
+        msg = Message(title,
+                      sender="noreply@kompetisi.id",
+                      recipients=target)
+        msg.html = render_template('email/index.html', body=Markup(body))
+        return app.mail.send(msg)
+    except NameError:
+        print("[ERROR] Error sending email....")
+        print(NameError)
+        return True
